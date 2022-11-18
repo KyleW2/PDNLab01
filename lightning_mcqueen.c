a0147556c0790d4a6a84f98e097623dcee040d20
Kyle Weidner <kyle.j.weidner-1@ou.edu>
2022-11-17 18:20:54 -0600

    Made it pthread

--- lightning_mcqueen.c
+++ lightning_mcqueen.c
@@ -1,21 +1,56 @@
+#include <pthread.h>
 
 #ifndef KA_CHOW
 #define KA_CHOW baseline_transpose
 #endif
 
-
-void VROOM(int i, int n, float *src, int rs_s, int cs_s, float *dst, int rs_d, int cs_d)
+// Struct for bundling argument
+typedef struct
 {
-  for (int j = 0; j < n; ++j)
+  int i;
+  int n;
+  float *src;
+  int rs_s;
+  int cs_s;
+  float *dst;
+  int rs_d;
+  int cs_d;
+} args;
+
+void VROOM(void *args)
+{
+  // Transpose the row
+  for (int j = 0; j < args->n; ++j)
   {
-    dst[j * rs_d + i * cs_d] = src[i * rs_s + j * cs_s];
+    args->dst[j * args->rs_d + args->i * args->cs_d] = src[args->i * args->rs_s + j * args->cs_s];
   }
 }
 
 void KA_CHOW(int m, int n, float *src, int rs_s, int cs_s, float *dst, int rs_d, int cs_d)
 {
+  // Create an array to keep track of thread ids
+  pthread_t *tids = malloc(sizeof(pthread_t) * m);
+
+  // Bundle arguments
+  args args;
+  args.n = n;
+  args.src = src;
+  args.rs_s = rs_s;
+  args.cs_s = cs_s;
+  args.dst = dst;
+  args.rs_d = rs_d;
+  args.cs_d = cs_d;
+
+  // Create threads and pass them the proper arguments
   for (int i = 0; i < m; ++i)
   {
-    VROOM(i, n, src, rs_s, cs_s, dst, rs_d, cs_d);
+    args.i = i;
+    pthread_create(&tid[i], NULL, VROOM, (void *)args);
+  }
+
+  // Join threads
+  for (int i = 0; i < m; ++i)
+  {
+    pthread_join(tids[m], NULL);
   }
 }
